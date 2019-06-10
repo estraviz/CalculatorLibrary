db3ca55d552b2359e5458226be7cfc455bac6d40
estraviz <javier.estraviz@gmail.com>
2019-06-11 01:05:46 +0200

    Add tests for division

--- test_calculator.py
+++ test_calculator.py
@@ -3,6 +3,7 @@ Unit tests for the calculator library
 """
 
 import calculator
+import pytest
 
 
 class TestCalculator:
@@ -15,3 +16,11 @@ class TestCalculator:
 
     def test_multiplication(self):
         assert 100 == calculator.multiply(10, 10)
+
+    def test_division_ok(self):
+        assert 2.5 == calculator.divide(10, 2)
+
+    def test_division_by_zero(self):
+        with pytest.raises(Exception) as excinfo:
+            raise Exception('ERROR: Division by Zero')
+        assert excinfo.value.message == 'ERROR: Division by Zero'
