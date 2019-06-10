73b5138424200b28e23dbec31f8bd2149ec75422
estraviz <javier.estraviz@gmail.com>
2019-06-11 01:09:21 +0200

    Add implementation for division

--- calculator.py
+++ calculator.py
@@ -2,6 +2,8 @@
 Calculator library containing basic math operations.
 """
 
+import pytest
+
 
 def add(first_term, second_term):
     return first_term + second_term
@@ -13,3 +15,10 @@ def subtract(first_term, second_term):
 
 def multiply(first_term, second_term):
     return first_term * second_term
+
+
+def divide(first_term, second_term):
+    try:
+        return first_term / second_term
+    except ZeroDivisionError as e:
+        pytest.fail(e, pytrace=True)
